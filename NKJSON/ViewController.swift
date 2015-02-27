9c4818f9dcde100ef7967cefcaffc97206e5d21c
Mihai Fratu <zeusent@msn.com>
2015-02-27 14:49:43 +0200

    Fixed demo app

--- NKJSON/ViewController.swift
+++ NKJSON/ViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var JSONString = "{\"data\": {\"name\": \"Mickey\", \"userID\": {\"series\": \"B\", \"number\": 2}, \"parents\": {\"father\": {\"name\": \"Florin\", \"userID\": {\"series\": \"B\", \"number\": 0}}}, \"languages\": [\"Romanian\", 123], \"siblings\": [{\"name\": \"Andu\", \"userID\": {\"series\": \"B\", \"number\": 1}}, {\"name\": \"Kitty\", \"userID\": {\"series\": \"F\", \"number\": 3}}]}}"
+    var JSONString = "{\"data\": {\"name\": \"Mickey\", \"id\": {\"series\": \"B\", \"number\": 2}, \"parents\": {\"father\": {\"name\": \"Florin\", \"id\": {\"series\": \"B\", \"number\": 0}}}, \"languages\": [\"Romanian\", 123], \"siblings\": [{\"name\": \"Andu\", \"userID\": {\"series\": \"B\", \"number\": 1}}, {\"name\": \"Kitty\", \"id\": {\"series\": \"F\", \"number\": 3}}]}}"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         if let user: User = NKJSON.parse(JSONString, key: "data") {
-            println("Hey \(user.name)!")
+            println("Hey \(user.name)! I see you have your ID with you. Series is \(user.id.series) and the number is \(user.id.number)")
             println("It seems like you have \(user.siblings.count) siblings. One is \(user.siblings[0].name) and the other one is \(user.siblings[1].name)")
             println("You can speak \(user.languages.count) languages and by that I mean \(user.languages[0]) and \(user.languages[1]), whatever that is...")
             println("Oh yeah! And your parents are as follows:")
